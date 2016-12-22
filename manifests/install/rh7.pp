188a95f1a0509b532e16ab3fad72986061075c20
P. Gomersbach <p.gomersbach@rely.nl>
2016-12-22 12:52:39 +0000

    add repo

--- manifests/install/rh7.pp
+++ manifests/install/rh7.pp
@@ -9,5 +9,19 @@ class beng_nrpe::install::rh7 {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  # install repository
+  yumrepo { 'epel-latest-7':
+    enabled    => 1,
+    descr      => 'Extra Packages for Enterprise Linux 7',
+    baseurl    => 'http://download.fedoraproject.org/pub/epel/7/x86_64',
+    mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=$basearch',
+    gpgcheck   => 0,
+  }
+
+  # install nrpe
+
+  # install plugins
+
+  # install configuration files
 
 }
